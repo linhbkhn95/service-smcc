4dcafb9f3a34b8f9cb2c1f2a1835d1bdc5168145
linhtd <linh.trinh@fss.com.vn>
2018-09-06 17:14:16 +0700

    u

--- config/env/development.js
+++ config/env/development.js
@@ -26,7 +26,7 @@ module.exports = {
   //   level: 'debug'
   // },
   // bpsUrl: 'http://192.168.1.92:1333', //link BPSService
-  bpsUrl: 'http://203.113.152.30:12000/', //link BPSService
+  bpsUrl: 'http://203.113.152.30:12000', //link BPSService
 
   // connections:{
   //   redispersistent: {
